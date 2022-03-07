aeec04411a97e87b35e06cdc4fabe300c5af4477
Pham Ky Nguyen <phamkynguyen753@gmail.com>
2022-03-07 11:04:56 +0700

    remove retrieve_all_scopes

--- api_base/urls.py
+++ api_base/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 v1_patterns = [
     path("auth/", include("api_authservices.urls")),
     path("users/", include("api_users.urls")),
-    path("oauth2/", include(("api_oauth2.urls", "oauth2"))),
     path("department/", include(("api_departments.urls"))),
     re_path(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
