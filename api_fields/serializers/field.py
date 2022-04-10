b6493500e89618f01ce1441bafa48200341391e0
nguyenrot <69588951+nguyenrot@users.noreply.github.com>
2022-04-10 16:23:25 +0700

    add name office, group

--- api_fields/serializers/field.py
+++ api_fields/serializers/field.py
@@ -3,6 +3,12 @@ from api_fields.models import Field
 
 
 class FieldSerializers(serializers.ModelSerializer):
+    name_parent_office = serializers.SerializerMethodField()
+
+    def get_name_parent_office(self, instance):
+        return instance.office.name
+
     class Meta:
         model = Field
         fields = "__all__"
+        extra_fields = ["name_parent_office"]
--- api_offices/serializers/office.py
+++ api_offices/serializers/office.py
@@ -3,6 +3,16 @@ from api_offices.models import Office, Group
 
 
 class OfficeSerializer(serializers.ModelSerializer):
+    name_parent_office = serializers.SerializerMethodField()
+    name_group = serializers.SerializerMethodField()
+
+    def get_name_parent_office(self, instance):
+        return instance.parent_office.name if instance.parent_office else None
+
+    def get_name_group(self, instance):
+        return instance.group.name if instance.group else "Các đơn vị cấp xã trực thuộc huyện"
+
     class Meta:
         model = Office
         fields = "__all__"
+        extra_fields = ["name_parent_office", "name_group"]
