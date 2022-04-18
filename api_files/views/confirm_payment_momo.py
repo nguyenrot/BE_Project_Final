739beaeb0571d64f789bba17f1f686c00420dced
nguyenrot <69588951+nguyenrot@users.noreply.github.com>
2022-04-18 22:32:43 +0700

    update request code momo

--- api_files/views/confirm_payment_momo.py
+++ api_files/views/confirm_payment_momo.py
@@ -10,15 +10,16 @@ class ConfirmPaymentView(viewsets.ViewSet):
 
     @action(detail=False, methods=["POST"], url_path="")
     def confirm_payment(self, request):
+        request_code = request.data.get("resultCode")
         order_id = request.data.get("orderId")
         request_id = request.data.get("requestId")
         payment = Payment.objects.filter(order_id=order_id, request_id=request_id).first()
         payment.response_time = request.data.get("responseTime")
         payment.message = request.data.get("message")
-        payment.result_code = request.data.get("resultCode")
+        payment.result_code = request_code
         payment.deep_link = request.data.get("deeplink")
         payment.qr_code_url = request.data.get("qrCodeUrl")
-        if payment.result_code == 0:
+        if request_code == "0":
             payment.record.status = 1
         payment.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
