a8a9add5283fa98bcac916b82a9425a02189c075
nguyenrot <69588951+nguyenrot@users.noreply.github.com>
2022-05-06 21:53:31 +0700

    statistic evaluate

--- api_evaluates/Views/evaluate.py
+++ api_evaluates/Views/evaluate.py
@@ -5,6 +5,7 @@ from api_evaluates.models import Evaluate
 from rest_framework.response import Response
 from rest_framework import status
 from api_files.models import ReceptionRecord
+from rest_framework.decorators import action
 
 
 class EvaluateView(viewsets.ModelViewSet):
@@ -27,7 +28,7 @@ class EvaluateView(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ("update", "partial_update", "destroy"):
             self.permission_classes = [TokenHasActionScope]
-        if self.action in ("retrieve", "list", "create"):
+        if self.action in ("retrieve", "list", "create", "statistic"):
             self.permission_classes = []
         return super(self.__class__, self).get_permissions()
 
@@ -68,3 +69,93 @@ class EvaluateView(viewsets.ModelViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return Response("Đã gửi đánh giá", status=status.HTTP_200_OK)
+
+    @action(methods=["get"], detail=False)
+    def statistic(self, request):
+
+        dict_question_1 = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+        dict_question_2 = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+        dict_question_3 = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+        dict_question_4 = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+        dict_question_5 = {
+            1: 0,
+            2: 0,
+            3: 0
+        }
+
+        queryset = Evaluate.objects.all()
+        for evaluate in queryset:
+
+            quantity_q_1, quantity_q_2, quantity_q_3, quantity_q_4, quantity_q_5 = 0, 0, 0, 0, 0
+            if evaluate.question_1 == 1:
+                quantity_q_1 += 1
+            elif evaluate.question_1 == 2:
+                quantity_q_2 += 1
+            elif evaluate.question_1 == 3:
+                quantity_q_3 += 1
+            dict_question_1[1] = dict_question_1[1] + quantity_q_1
+            dict_question_1[2] = dict_question_1[2] + quantity_q_2
+            dict_question_1[3] = dict_question_1[3] + quantity_q_3
+            print(dict_question_1)
+
+            quantity_q_1, quantity_q_2, quantity_q_3, quantity_q_4, quantity_q_5 = 0, 0, 0, 0, 0
+            if evaluate.question_2 == 1:
+                quantity_q_1 += 1
+            elif evaluate.question_2 == 2:
+                quantity_q_2 += 1
+            elif evaluate.question_2 == 3:
+                quantity_q_3 += 1
+            dict_question_2[1] = dict_question_2[1] + quantity_q_1
+            dict_question_2[2] = dict_question_2[2] + quantity_q_2
+            dict_question_2[3] = dict_question_2[3] + quantity_q_3
+
+            quantity_q_1, quantity_q_2, quantity_q_3, quantity_q_4, quantity_q_5 = 0, 0, 0, 0, 0
+            if evaluate.question_3 == 1:
+                quantity_q_1 += 1
+            elif evaluate.question_3 == 2:
+                quantity_q_2 += 1
+            elif evaluate.question_3 == 3:
+                quantity_q_3 += 1
+            dict_question_3[1] = dict_question_3[1] + quantity_q_1
+            dict_question_3[2] = dict_question_3[2] + quantity_q_2
+            dict_question_3[3] = dict_question_3[3] + quantity_q_3
+
+            quantity_q_1, quantity_q_2, quantity_q_3, quantity_q_4, quantity_q_5 = 0, 0, 0, 0, 0
+            if evaluate.question_4 == 1:
+                quantity_q_1 += 1
+            elif evaluate.question_4 == 2:
+                quantity_q_2 += 1
+            elif evaluate.question_4 == 3:
+                quantity_q_3 += 1
+            dict_question_4[1] = dict_question_4[1] + quantity_q_1
+            dict_question_4[2] = dict_question_4[2] + quantity_q_2
+            dict_question_4[3] = dict_question_4[3] + quantity_q_3
+
+            quantity_q_1, quantity_q_2, quantity_q_3, quantity_q_4, quantity_q_5 = 0, 0, 0, 0, 0
+            if evaluate.question_5 == 1:
+                quantity_q_1 += 1
+            elif evaluate.question_5 == 2:
+                quantity_q_2 += 1
+            elif evaluate.question_5 == 3:
+                quantity_q_3 += 1
+            dict_question_5[1] = dict_question_5[1] + quantity_q_1
+            dict_question_5[2] = dict_question_5[2] + quantity_q_2
+            dict_question_5[3] = dict_question_5[3] + quantity_q_3
+        list_question = [dict_question_1, dict_question_2, dict_question_3, dict_question_4, dict_question_5]
+        return Response({"total_evaluate": queryset.count(), "results": list_question})
